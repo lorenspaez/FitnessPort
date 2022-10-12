2578eed74e7ce8d327d5d61c135ff26f5176de8f
lorenspaez <54387407+lorenspaez@users.noreply.github.com>
2022-10-12 16:57:06 -0300

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const host = process.env.HOST || '0.0.0.0';
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(PORT, host);
   console.log("Running on: " + host + ":" + PORT + "/");
   
